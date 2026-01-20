d307b2a75b93d3ca29d9973ad0617825f01c3731
zack <8156341+yu-gui-wan@user.noreply.gitee.com>
2026-01-20 15:58:59 +0800

    python file test

--- python_file_use/7-python程序如何传参.py
+++ python_file_use/7-python程序如何传参.py
@@ -3,7 +3,7 @@ import sys
 
 
 # print(type(sys.argv))
-# print(sys.argv)
+#print(sys.argv)
 def write_hello(file_path):
     file = open(file_path, 'w+', encoding='utf8')
     file.write('hello')
@@ -11,4 +11,5 @@ def write_hello(file_path):
 
 
 if __name__ == '__main__':
-    write_hello(sys.argv[1])
+    print(sys.argv)
+    write_hello(sys.argv[1])#直接运行错误是因为没有传递参数，在运行编辑配置中添加参数即可
