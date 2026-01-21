ac999b72f48991b191ecca2f31bd4d27f22b6d5b
zack <8156341+yu-gui-wan@user.noreply.gitee.com>
2026-01-21 20:27:17 +0800

    2026.1.22

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -1,5 +1,7 @@
 #include<stdio.h>
 #include<math.h>
+#include <cstdlib>
+#include <algorithm>
 
 //正整数和负整数的最大计数
 int maximumCount(int* nums, int numsSize) {
@@ -56,6 +58,31 @@ int maximumCount(int* nums, int numsSize) {
     // 返回正负数中较大的数量
     return pos > neg ? pos : neg;
 }
+//与对应负数同时存在的最大正整数
+int findMaxK(int* nums, int numsSize) {
+    int* arr_pos = (int*)malloc(sizeof(int) * 1001);
+    int* arr_neg = (int*)malloc(sizeof(int) * 1001);
+    int i;
+    for (i = 0; i < 1001; i++) {
+        arr_neg[i] = 0;
+        arr_pos[i] = 0;
+    }
+    for (i = 0; i < numsSize; i++) {
+        if (nums[i] > 0) {
+            arr_pos[nums[i]] = 1;
+        }
+        else {
+            arr_neg[-nums[i]] = 1;
+        }
+    }
+    for (i = 1000; i >= 0; i--) {
+        if (arr_neg[i] == 1 && arr_pos[i] == 1) {
+            return i;
+        }
+    }
+    return -1;
+}
+
 
 
 
