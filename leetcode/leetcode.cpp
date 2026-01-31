f5bbb8c4ca4f15bf0f7e1eb1af0f43e684a1df02
zack <8156341+yu-gui-wan@user.noreply.gitee.com>
2026-01-31 22:29:05 +0800

    2026.1.31

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -218,7 +218,31 @@ struct ListNode* mergeNodes(struct ListNode* head) {
 
     return head;
 }
-
+//ÒÆ³ıÁ´±íÔªËØ
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+struct ListNode* removeElements(struct ListNode* head, int val) {
+    struct ListNode* p = head, * temp;
+    if (head == NULL)return head;
+    while (head != NULL && head->val == val) {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+    if (!head)return head;
+    p = head;
+    while (p->next) {
+        if (p->next->val == val) {
+            p->next = p->next->next;
+        }
+        else {
+            p = p->next;
+        }
+    }
+    return head;
+}
 
 
 
